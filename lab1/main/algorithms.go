c5dd76903d0f9e5097a798c82dbae60b352d97ce
kutyrov <kutyrov.andrey@gmail.com>
2022-04-01 16:15:20 +0300

    залита первая лабораторная

---
+++ lab1/main/algorithms.go
@@ -0,0 +1,276 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Step struct {
+	AChoice        int
+	BChoice        int
+	AWin           []float64
+	BLose          []float64
+	EvaluationUp   float64
+	EvaluationDown float64
+	Epsilon        float64
+}
+
+func NewStep() Step {
+	s := Step{
+		AChoice:        0,
+		BChoice:        0,
+		AWin:           make([]float64, 0),
+		BLose:          make([]float64, 0),
+		EvaluationUp:   0.0,
+		EvaluationDown: 0.0,
+		Epsilon:        0.0,
+	}
+	return s
+}
+
+// при равных выигрышах функция выберет случаную стратегию
+func RandChoiceA(data []float64) int {
+	max := MaxFromSlice(data)
+	indexes := make([]int, 0)
+	for index := range data {
+		if data[index] == max {
+			indexes = append(indexes, index)
+		}
+	}
+	randomIndex := RandomNumber(len(indexes))
+	return indexes[randomIndex]
+}
+
+// при равных проигрышах функция выберет случайную стратегию
+func RandChoiceB(data []float64) int {
+	min := MinFromSlice(data)
+	indexes := make([]int, 0)
+	for index := range data {
+		if data[index] == min {
+			indexes = append(indexes, index)
+		}
+	}
+	randomIndex := RandomNumber(len(indexes))
+	return indexes[randomIndex]
+}
+
+// более-менее красивый вывод таблицы шагов
+func PrintSteps(steps []Step) {
+	fmt.Printf("	A	B	")
+	for index := range steps[0].AWin {
+		fmt.Printf("x%d		", index+1)
+	}
+	for index := range steps[0].BLose {
+		fmt.Printf("y%d		", index+1)
+	}
+	fmt.Printf("EvUp	EvDn	Eps\n")
+	for index := range steps {
+		fmt.Printf("N%d	%d	%d	", index+1, steps[index].AChoice+1, steps[index].BChoice+1)
+		for jindex := range steps[index].AWin {
+			fmt.Printf("%.2f		", steps[index].AWin[jindex])
+		}
+		for jindex := range steps[index].BLose {
+			fmt.Printf("%.2f		", steps[index].BLose[jindex])
+		}
+		fmt.Printf("%.2f	%.2f	%.2f\n",
+			steps[index].EvaluationUp,
+			steps[index].EvaluationDown,
+			steps[index].Epsilon,
+		)
+	}
+}
+
+// функия рассичитывает погрешность эпсилон по всей таблице шагов
+func CalculateEpsion(steps []Step) float64 {
+	maxs := make([]float64, len(steps))
+	mins := make([]float64, len(steps))
+	for index := range steps {
+		maxs[index] = steps[index].EvaluationUp
+		mins[index] = steps[index].EvaluationDown
+	}
+	minmax := MinFromSlice(maxs)
+	maxmin := MaxFromSlice(mins)
+	return minmax - maxmin
+}
+
+// функция находит максимальное число в слайсе
+func MaxFromSlice(s []float64) float64 {
+	if len(s) == 0 {
+		return 0
+	}
+	max := s[0]
+	for i := range s {
+		if s[i] > max {
+			max = s[i]
+		}
+	}
+	return max
+}
+
+// функция находит минимальное число в слайсе
+func MinFromSlice(s []float64) float64 {
+	if len(s) == 0 {
+		return 0
+	}
+	min := s[0]
+	for i := range s {
+		if s[i] < min {
+			min = s[i]
+		}
+	}
+	return min
+}
+
+// функция генерирует случайное число в интервале [0,data)
+func RandomNumber(data int) int {
+	if data <= 0 {
+		return 0
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(data)
+}
+
+// функция пытается найти седловую точку матрицы и возвращает ошибку если её нет
+func SaddlePoint(matrix [][]float64) (int, int, float64, error) {
+	maxs := make([]float64, len(matrix[0]), len(matrix[0]))
+	mins := make([]float64, len(matrix), len(matrix))
+
+	// найдем минимумы по строкам
+	for index := range matrix {
+		mins[index] = matrix[index][0]
+		for jindex := range matrix[0] {
+			if matrix[index][jindex] < mins[index] {
+				mins[index] = matrix[index][jindex]
+			}
+		}
+	}
+
+	// найдем максимумы по столбцам
+	for jindex := range matrix[0] {
+		maxs[jindex] = matrix[0][jindex]
+		for index := range matrix {
+			if matrix[index][jindex] > maxs[jindex] {
+				maxs[jindex] = matrix[index][jindex]
+			}
+		}
+	}
+
+	// найдем minmax и maxmin
+	minmax := maxs[0]
+	minmaxIndex := 0
+	for index := range maxs {
+		if maxs[index] < minmax {
+			minmax = maxs[index]
+			minmaxIndex = index
+		}
+	}
+
+	maxmin := mins[0]
+	maxminIndex := 0
+	for index := range mins {
+		if mins[index] > maxmin {
+			maxmin = mins[index]
+			maxminIndex = index
+		}
+	}
+
+	if minmax == maxmin {
+		return maxminIndex + 1, minmaxIndex + 1, minmax, nil
+	} else {
+		return -1, -1, 0, errors.New("Седловой точки нет")
+	}
+}
+
+func BrownRobinson(matrix [][]float64) ([]float64, []float64, float64) {
+	strategics := len(matrix)
+	steps := make([]Step, 0)
+	stepNumber := 0
+
+	// Делаем первый шаг алгоритма (заполнение первой строки таблицы)
+	steps = append(steps, NewStep())
+	steps[0].AChoice = RandomNumber(strategics)
+	steps[0].BChoice = RandomNumber(strategics)
+	for index := 0; index < strategics; index++ {
+		steps[0].AWin = append(steps[0].AWin, matrix[index][steps[0].AChoice])
+		steps[0].BLose = append(steps[0].BLose, matrix[steps[0].BChoice][index])
+
+	}
+	steps[0].EvaluationUp = MaxFromSlice(steps[0].AWin)
+	steps[0].EvaluationDown = MinFromSlice(steps[0].BLose)
+	steps[0].Epsilon = CalculateEpsion(steps)
+
+	// остальные строки таблицы заполняем в цикле
+	// условие остановки соглано заданию по погрешности
+	for steps[stepNumber].Epsilon > 0.1 {
+		stepNumber += 1
+		steps = append(steps, NewStep())
+		steps[stepNumber].AChoice = RandChoiceA(steps[stepNumber-1].AWin)
+		steps[stepNumber].BChoice = RandChoiceB(steps[stepNumber-1].BLose)
+		for index := 0; index < strategics; index++ {
+			steps[stepNumber].AWin = append(steps[stepNumber].AWin,
+				steps[stepNumber-1].AWin[index]+matrix[index][steps[stepNumber].BChoice])
+			steps[stepNumber].BLose = append(steps[stepNumber].BLose,
+				steps[stepNumber-1].BLose[index]+matrix[steps[stepNumber].AChoice][index])
+		}
+		steps[stepNumber].EvaluationUp =
+			MaxFromSlice(steps[stepNumber].AWin) / (float64(stepNumber) + 1)
+		steps[stepNumber].EvaluationDown =
+			MinFromSlice(steps[stepNumber].BLose) / (float64(stepNumber) + 1)
+		steps[stepNumber].Epsilon = CalculateEpsion(steps)
+
+	}
+
+	// Получаем итоговое решение из таблицы
+	value := (steps[stepNumber].EvaluationUp + steps[stepNumber].EvaluationDown) / 2
+	aStrategy := make([]float64, strategics)
+	bStrategy := make([]float64, strategics)
+	for index := range steps {
+		aStrategy[steps[index].AChoice] += 1
+		bStrategy[steps[index].BChoice] += 1
+	}
+	for index := range aStrategy {
+		aStrategy[index] /= float64(stepNumber + 1)
+	}
+	for index := range bStrategy {
+		bStrategy[index] /= float64(stepNumber + 1)
+	}
+
+	PrintSteps(steps)
+
+	return aStrategy, bStrategy, value
+}
+
+func AnalyticalMethod(matrix [][]float64) ([]float64, []float64, float64, error) {
+	if len(matrix) != len(matrix[0]) {
+		return nil, nil, 0, errors.New("Подана неправильная матрица")
+	}
+
+	x := make([]float64, len(matrix), len(matrix))
+	y := make([]float64, len(matrix), len(matrix))
+
+	// вычислим одинаковый для x, y и v знаменатель
+	denomiator := 0.0
+	for index := range matrix {
+		for jindex := range matrix {
+			denomiator += matrix[index][jindex]
+		}
+	}
+
+	// вычисляем x* и y*
+	for index := range matrix {
+		for jindex := range matrix {
+			x[index] += matrix[jindex][index]
+			y[index] += matrix[index][jindex]
+		}
+		x[index] /= denomiator
+		y[index] /= denomiator
+	}
+
+	// вычисляем цену игры v
+	var v float64 = 1 / denomiator
+	return x, y, v, nil
+
+}
---
+++ lab1/main/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//const defaultPath = "data/example.txt"
+
+const defaultPath = "data/12.txt"
+
+//const defaultPath = "data/temp2.txt"
+
+// удобный вывод слайса
+func printSlice(data []float64) {
+	if len(data) == 0 {
+		return
+	}
+	fmt.Printf("%.2f", data[0])
+	for index := 1; index < len(data); index++ {
+		fmt.Printf(" %.2f", data[index])
+	}
+	fmt.Printf("\n")
+}
+
+// функция считывания размеров матрицы из файла
+func getSizes(path string) (int, int, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	rows, columns := 0, 0
+	for scanner.Scan() {
+		numbers := strings.Fields(scanner.Text())
+		if len(numbers) != 2 {
+			return 0, 0, errors.New("неправильное число аргументов в первой строке")
+		}
+
+		rows, err = strconv.Atoi(numbers[0])
+		if err != nil {
+			return 0, 0, errors.New("первый аргумент это не число")
+		}
+
+		columns, err = strconv.Atoi(numbers[1])
+		if err != nil {
+			return 0, 0, errors.New("второй аргумент это не число")
+		}
+
+		if rows <= 0 || columns <= 0 {
+			return 0, 0, errors.New("размер матрицы не может быть отрицательным")
+		}
+		break
+	}
+
+	return rows, columns, nil
+
+}
+
+// функция считывания матрицы из файла
+// возможно стоит сделать композицию с getSizes
+func getMatrix(path string, rows, columns int) ([][]float64, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	index := -1
+	matrix := make([][]float64, rows, rows)
+	for jindex := range matrix {
+		matrix[jindex] = make([]float64, columns, columns)
+	}
+
+	for scanner.Scan() {
+		numbers := strings.Fields(scanner.Text())
+		index += 1
+
+		if index == 0 {
+			continue
+		}
+
+		if index > rows {
+			return nil, errors.New("Слишком много строк")
+		}
+		if len(numbers) != columns {
+			return nil, errors.New("Неправильное число столбцов в матрице")
+		}
+		for jindex, number := range numbers {
+			temp, err := strconv.Atoi(number)
+			matrix[index-1][jindex] = float64(temp)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	if index < rows {
+		return nil, errors.New("неправильное число строк")
+	}
+	return matrix, nil
+}
+
+func main() {
+
+	// считываем данные
+	path := ""
+	fmt.Println("Введите имя файла или нажмите enter")
+	fmt.Scanf("%s\n", &path)
+	if path == "" {
+		path = defaultPath
+	}
+
+	rows, columns, error := getSizes(path)
+	if error != nil {
+		panic(error)
+	}
+
+	matrix, error := getMatrix(path, rows, columns)
+	if error != nil {
+		panic(error)
+	}
+
+	fmt.Printf("Размеры матрицы %d на %d\n", rows, columns)
+	fmt.Println(rows, columns)
+	fmt.Println("\nСама матрица:")
+	for index := range matrix {
+		for jindex := range matrix[index] {
+			fmt.Printf("%d ", int(matrix[index][jindex]))
+		}
+		fmt.Println()
+	}
+
+	// проверим наличие седловой точки
+	aDominant, bDominant, value, err := SaddlePoint(matrix)
+	if err == nil {
+		fmt.Printf("Седловая точка есть. Игрок А должен использовать стратегию %d,"+
+			"игрок B должен использовать стратегию %d, цена игры равна %.2f\n",
+			aDominant,
+			bDominant,
+			value,
+		)
+	} else {
+		fmt.Println("Седловой точки нет")
+	}
+
+	// Аналитичесий метод
+	fmt.Println("\nРешаем аналитическим методом")
+	C, err := InverseMatrix(matrix)
+	if err != nil {
+		panic(err)
+	}
+
+	x, y, v, _ := AnalyticalMethod(C)
+
+	fmt.Printf("Стратегия игрока А: ")
+	printSlice(x)
+	fmt.Printf("Стратегия игрорка B: ")
+	printSlice(y)
+	fmt.Printf("Цена игры равна %.2f\n", v)
+
+	// Алгоритм Брауна-Робинсон
+	fmt.Println("\nРешаем методом Брауна-Робинсон")
+	aStrategy, bStrategy, value := BrownRobinson(matrix)
+	fmt.Printf("Стратегия игрока А: ")
+	printSlice(aStrategy)
+	fmt.Printf("Стратегия игрорка B: ")
+	printSlice(bStrategy)
+	fmt.Printf("Цена игры равна %.2f\n", value)
+}
