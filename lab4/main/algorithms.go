73a4aa09901364257e616783afd234e3ac77a189
kutyrov <kutyrov.andrey@gmail.com>
2022-04-18 11:06:21 +0300

    написана функция генерации дерева

--- lab4/main/algorithms.go
+++ lab4/main/algorithms.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 type node struct {
-	parent   int
+	parent   *node
+	player   int
 	children []*node
+	value    int
 	data     []int
 }
 
@@ -19,6 +22,16 @@ func randInt(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
+func randBool() bool {
+	number := rand.Intn(2)
+	if number == 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+//генерирует случайный слайс длины n
 func randSlice(n int) []int {
 	if n < 0 {
 		return nil
@@ -30,7 +43,66 @@ func randSlice(n int) []int {
 	return data
 }
 
-func generateTree(height, players int, strategies []int) node {
+func generateTree(height int, strategies []int) *node {
+	// проверяем входные параметры
+	if height <= 0 {
+		return nil
+	}
+	for _, value := range strategies {
+		if value <= 0 {
+			return nil
+		}
+	}
 	rand.Seed(time.Now().UnixNano())
-	return node{}
+	// обозначаем корень дерева
+	head := node{nil, -1, nil, 0, nil}
+	head.children = make([]*node, len(strategies))
+
+	// на первом уровне должны присутстовать все игроки
+	for i := range strategies {
+		head.children[i] = &node{&head, i, nil, 0, nil}
+	}
+	fmt.Println(head.children)
+	//осталось сгенерировать height-1 уровень
+	thisLevel := head.children
+	//fmt.Println(thisLevel)
+
+	for i := 0; i < height-1; i++ {
+		nextLevel := make([]*node, 0)
+		// убедились, что на следующем уровне будут вершины
+		for len(nextLevel) == 0 && i != height-2 {
+			for _, n := range thisLevel {
+				n.children = make([]*node, strategies[n.player])
+				for index := 0; index < strategies[n.player]; index++ {
+					if randBool() {
+						n.children[index] = &node{
+							n,
+							(n.player + 1) % len(strategies),
+							nil,
+							0,
+							nil,
+						}
+						nextLevel = append(nextLevel, n.children[index])
+					}
+				}
+			}
+		}
+		thisLevel = make([]*node, len(nextLevel))
+		copy(thisLevel, nextLevel)
+	}
+	// в коненые вершины записываем случайные выигрыши
+	thisLevel = head.children
+	for i := 0; i < height-1; i++ {
+		nextLevel := make([]*node, 0)
+		for _, n := range thisLevel {
+			if n.children == nil {
+				n.data = randSlice(len(strategies))
+			} else {
+				nextLevel = append(nextLevel, n)
+			}
+		}
+		thisLevel = make([]*node, len(nextLevel))
+		copy(thisLevel, nextLevel)
+	}
+	return &head
 }
--- lab4/main/main.go
+++ lab4/main/main.go
@@ -1,9 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 const (
 	minRand     = -5
 	maxRand     = 25
 	defaultPath = "data/12.txt"
 )
 
-func main() {}
+func main() {
+	height := 0
+	players := 0
+	strategies := make([]int, 0)
+	// считываем данные с файла
+	file, err := os.Open(defaultPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	index := 0
+	for scanner.Scan() {
+		numbers := strings.Fields(scanner.Text())
+		if index == 0 {
+			height, _ = strconv.Atoi(numbers[0])
+			players, _ = strconv.Atoi(numbers[1])
+		} else if index == 1 {
+			for i := 0; i < players; i++ {
+				number, _ := strconv.Atoi(numbers[i])
+				strategies = append(strategies, number)
+			}
+		} else {
+			panic("Неправильный формат файла")
+		}
+		index += 1
+	}
+	// fmt.Println(height, players, strategies)
+	head := generateTree(height, strategies)
+	fmt.Println(*head)
+}
